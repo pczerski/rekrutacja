5f44fe6551b1fc8896e2623c7cf54c93e5c1a4f7
Piotr Czerski <blater@poczta.fm>
2017-07-15 16:00:49 +0200

    koniec ćwiczeń

--- js/script.js
+++ js/script.js
@@ -1,27 +1,42 @@
 'use strict';
 
 $(function () {
-    for (var i = 1; i <= 5; i++) {
 
-        $.getJSON('https://jsonplaceholder.typicode.com/users/' + i, function (data) {
-            console.log(data);
-            console.log(data.name);
-            console.log(data.address.street);
+    function getUsersData(init) {
+        for (var i = init; i <= init+4; i++) {
 
-            var parName = document.createElement('p');
-            var parStreet = document.createElement('p');
-            parName.innerHTML = 'Imię i nazwisko: ' + data.name;
-            parStreet.innerHTML = 'Ulica: ' + data.address.street;
-            
-            parName.className += 'user-name';
-            parStreet.className += 'user-street';
-            //console.log(parName);
-            //console.log(parStreet);
+            $.getJSON('https://jsonplaceholder.typicode.com/users/' + i, function (data) {
+                console.log(data);
+                console.log(data.name);
+                console.log(data.address.street);
 
-            $('#users-data').append(parName);
-            $('#users-data').append(parStreet);
+                var parId = document.createElement('p');
+                var parName = document.createElement('p');
+                var parStreet = document.createElement('p');
+                
+                parId.innerHTML = 'ID: ' + data.id;
+                parName.innerHTML = 'Imię i nazwisko: ' + data.name;
+                parStreet.innerHTML = 'Ulica: ' + data.address.street;
 
-        });
-    }
+                parId.className += 'user-id';
+                parName.className += 'user-name';
+                parStreet.className += 'user-street';
+                //console.log(parName);
+                //console.log(parStreet);
 
+                $('#users-data').append(parId);
+                $('#users-data').append(parName);
+                $('#users-data').append(parStreet);
+
+            });
+        }
+    };
+    getUsersData(1);
+    
+    $('#load-users').on('click', function () {
+        var usersOnList = $('.user-name').length; // Ile jest w sumie elementów    
+        
+        getUsersData(usersOnList + 1);
+    });
+    
 });
